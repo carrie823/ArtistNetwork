c7c6a3bec80742dbf98d0abf02bf16733e24254c
Carrie Ng <114375756+carrie823@users.noreply.github.com>
2023-06-13 16:31:19 -0400

    update AddArtwork.jsx

--- frontend/my-react-app/src/components/AddArtwork.jsx
+++ frontend/my-react-app/src/components/AddArtwork.jsx
@@ -1,16 +1,20 @@
 // import { Link } from "react-router-dom";
 import React, { useRef, useState, useEffect } from 'react';
 import HeaderMain from "./HeaderMain";
-import  Footer  from "./footer";
+import Footer from "./footer";
+import { useNavigate } from 'react-router-dom';
 
 export default function AddArtwork() {
-
+  const nav = useNavigate();
   const imageRef = useRef();
   const titleRef = useRef();
+  const nameRef = useRef();
   const descriptionRef = useRef();
   const [titleVal, setTitle] = useState("");;
   const [descriptionVal, setDescription] = useState("");
   const [imageVal, setImage] = useState("");
+  // const [nameVal, setName] = useState("");
+
 
   // const [titleDisplay, setTitleDisplay] = useState("");
 
@@ -24,10 +28,11 @@ export default function AddArtwork() {
 
     fetch(url, {
       method: methodX,
+      credentials: 'include',
       body: formdata
     }).then(response => {
       if (response.status === 200) {
-        // nav("/")
+        nav("/studiospace")
         return response.json()
       }
       return response.json();
@@ -35,12 +40,14 @@ export default function AddArtwork() {
   }
 
   function sendData() {
+    let username = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     let title = titleVal;
     let desc = descriptionVal;
     let image = imageVal[0];
+    // let name = nameVal;
     console.log(image)
 
-    let bodyX = { "title": title, "description": desc, "image": image }
+    let bodyX = { "title": title, "description": desc, "image": image, "username": username }
 
     sendFiles("POST", "http://localhost:3001/api/images/arts", bodyX, false, function (event) {
       if (event) {
@@ -48,7 +55,6 @@ export default function AddArtwork() {
       }
     });
   }
-
 
   return (
     <main id="artwork-form">
@@ -60,6 +66,10 @@ export default function AddArtwork() {
             <input type="file" ref={imageRef} onChange={(event) => { setImage(event.target.files) }} />
             {/* <button type="submit">Upload</button> */}
           </div>
+          {/* <div class="fields">
+            <label for="title" >Username:</label>
+            <input type="text" ref={nameRef} onChange={(event) => { setName(event.target.value) }} />
+          </div> */}
           <div class="fields">
             <label for="title" >Title:</label>
             <input type="text" ref={titleRef} onChange={(event) => { setTitle(event.target.value) }} />
@@ -77,7 +87,7 @@ export default function AddArtwork() {
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </main>
   );
 }
